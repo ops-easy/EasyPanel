431df742c00a45d47cd7de051f26682e1a422426
xiao ran <xiaoran@butnoice.com>
2026-05-20 14:20:12 +0800

    refactor: remove legacy internal backend scaffolding

--- api/internal/platform_kv_file.go
+++
@@ -1,9 +0,0 @@
-package internal
-
-import "kube-bt-sync/pkg/platformkv"
-
-type PlatformKVFile = platformkv.File
-
-func newPlatformKVFile(dataDir string) (*PlatformKVFile, error) {
-	return platformkv.NewFile(dataDir)
-}
--- api/internal/platform_kv_iface.go
+++
@@ -1,6 +0,0 @@
-package internal
-
-import "kube-bt-sync/pkg/platformkv"
-
-// PlatformKV 平台键值（cloud_hosts、向导状态等）；实现可为本地 JSON 文件或 MySQL。
-type PlatformKV = platformkv.Store
