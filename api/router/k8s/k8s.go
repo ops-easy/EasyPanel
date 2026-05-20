b922daee25fbb004dff4fb7a5cd21ac1b116b47e
xiao ran <xiaoran@butnoice.com>
2026-05-20 11:56:30 +0800

    refactor: route kubernetes from autoops router

--- api/router/k8s/k8s.go
+++ api/router/k8s/k8s.go
@@ -1,12 +1,12 @@
 package k8s
 
 import (
+	"kube-bt-sync/api/k8s/controller"
 	core "kube-bt-sync/internal"
 
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(api *gin.RouterGroup, app *core.ServerApp) {
-	_ = api
-	_ = app
+	controller.New(app).RegisterRoutes(api)
 }
